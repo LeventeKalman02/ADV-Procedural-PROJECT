4704f1cd3bbf522abe82ed134afb11a076ca350f
EnhancedTarget <levente.kalman922@gmail.com>
2023-04-17 17:52:22 +0100

    Add project files.

---
+++ Main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+	int idNum;
+	char firstName;
+	char surName;
+	int year;
+	float monthlyRental;
+	char email;
+	bool available;
+};
+
+void main() {
+		
+}

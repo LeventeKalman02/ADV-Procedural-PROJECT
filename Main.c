aec744580df5e6ff9b1fdd089fc45aa05d4acd75
EnhancedTarget <levente.kalman922@gmail.com>
2023-04-17 18:22:01 +0100

    setup

--- Main.c
+++ Main.c
@@ -10,9 +10,47 @@ typedef struct {
 	int year;
 	float monthlyRental;
 	char email;
-	bool available;
-};
+	bool accomAvailable;
+	int roomAvailable;
+	bool pets;
+	int stayDuration;
+}AccomodationsT;
+
+void Menu();
+void addAccomodation();
+void displayAccomodations();//both all accomodations and specific ones if chosen
+void updateAccomodations();
+void deleteAccomodations();
+void generalStats();
 
 void main() {
-		
+	
+}
+
+void Menu() {
+
+}
+
+void addAccomodation() {
+
+}
+
+void displayAccomodations() {
+
+}
+
+void updateAccomodations() {
+
+}
+
+void deleteAccomodations() {
+
+}
+
+void deleteAccomodations() {
+
+}
+
+void generalStats() {
+
 }

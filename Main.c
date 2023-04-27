01eef63f4c303aaca7728894f0d1408375a01e06
EnhancedTarget <levente.kalman922@gmail.com>
2023-04-27 20:35:20 +0100

    done

--- Main.c
+++ Main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <stdbool.h>
 
-typedef struct node{
-	int idNum;
+typedef struct Node {
+	int idNum;//unique identifier
 	char firstName[20];
 	char surName[20];
 	int year;
@@ -12,118 +12,175 @@ typedef struct node{
 	char email[50];
 	char accomAvailable[20];//type of accommodation: single room, apartment, bungalow, two story
 	int roomAvailable;//1-4 and >4
-	bool pets;//are pets allowed
+	int pets;//are pets allowed
 	int stayDuration;// one day, <3 days, <7 days, >7 days
 
-	struct node* next;
-}AccomodationsT;
+	struct Node* next;
+};
 
-AccomodationsT* head = NULL;
+//searching
+struct node* find(int target);//find function
 
-//function declarations
+//adding & deleting
+void insert();//inserts node into the linked list
+void deleteAccommodation();//delete an accommodation from the linked list
 
-void AddNodeData(AccomodationsT** first, int id, char n[20], char s[20], int year, float monthlyRent, char e[50], char accomAvailable[20], int roomAvailable, bool pet, int duration);//adds the node to the linked list
-void addAccommodation(AccomodationsT** first);//add an accomodation
-struct node* find(int key);//finds the specific accommodation based on the ID
-void displayAccommodations(AccomodationsT** first);//both all accomodations and specific ones if chosen
-void updateAccommodations(AccomodationsT** first);//allows the admin to update the accommodation details
-void deleteAccommodations(AccomodationsT** first, int key);//delete accommodation from the linked list
-void generalStats(AccomodationsT** first);//prints general statistics to the screen
-void saveToFile(AccomodationsT** first);//print all of the accommodations in the list to a file
-void listRental(AccomodationsT** first);//list all accommodations in order of monthly rental
+//displaying
+void displayAll();//display all of the nodes
+void displaySpecific();//display specific node
+void display(struct Node* temp);//display function
+
+//updating & saving
+void saveToFile(struct Node* temp);//save the linked list to a file
+void loadFromFile();//load data from the file
+void updateAccommodation();//allows the admin to update the accommodation details
+
+//Stats
+int getTotalRooms();//gets the % of total rooms
+
+int totalNodes = 0;
+struct Node* head = NULL;
 
 void main() {
-	AccomodationsT* currentN;
-	AccomodationsT* previousN;
 
-	int menuChoice = 0, key;
+	/*char s[20];
+	char n[20];
+	char e[20];
+	char accom[20];*/
 
-	printf("Enter 1 to add a new accommodation:\n");
-	printf("Enter 2 to delete an existing accommodation:\n");
-	printf("Enter 3 to display an accommodation:\n");
-	printf("Enter 4 to update an existing accommodation:\n");
-	printf("Enter 5 to list the general stats of an accommodation:\n");
-	printf("Enter 6 to list the monthly rental income of an accommodation:\n");
+
+	//populating the linked list manually
+	// 
+	//The first
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node) * 1);
+	totalNodes++;
+	newNode->idNum = totalNodes;
+	newNode->next = NULL;
+	strcpy(newNode->firstName, "amy");
+	strcpy(newNode->surName, "McDonagh");
+	newNode->year = 42069;
+	newNode->monthlyRental = 123456789;
+	strcpy(newNode->email, "my123@googlemail.com");
+	strcpy(newNode->accomAvailable, "single");
+	newNode->roomAvailable = 69;
+	newNode->pets = 0;
+	newNode->stayDuration = 4;
+	head = newNode;
+
+	//The second
+	struct Node* newNode2 = (struct Node*)malloc(sizeof(struct Node) * 1);
+	totalNodes++;
+	newNode2->idNum = totalNodes;
+	newNode2->next = NULL;
+	strcpy(newNode2->firstName, "Mr");
+	strcpy(newNode2->surName, "meseeks");
+	newNode2->year = 42069;
+	newNode2->monthlyRental = 123456789;
+	strcpy(newNode2->email, "meseeks@intergalacticmail.everywhere");
+	strcpy(newNode2->accomAvailable, "bungalow");
+	newNode2->roomAvailable = 2;
+	newNode2->pets = 1;
+	newNode2->stayDuration = 23;
+	head->next = newNode2;
+
+	int menuChoice = 0;
+
+	printf("Enter 1 to add a new accommodation\n");
+	printf("Enter 2 to delete an existing accommodation\n");
+	printf("Enter 3 to display all accommodation\n");
+	printf("Enter 4 to display a specific accommodation\n");
+	printf("Enter 5 to update an existing accommodation\n");
+	printf("Enter 6 to save all accommodations to a file\n");
+	printf("Enter 7 to get the percentage of each room that is available\n");
 	scanf("%d", &menuChoice);
 
 	while (menuChoice != -1) {
-		if (menuChoice == 1) {//add a new accomodation
-			addAccommodation(&head);
+		if (menuChoice == 1) {//add an accommodation
+			insert();
 		}
-		else if (menuChoice == 2) {//delete an accomodation
-			printf("Please enter the ID of the accommodation you wish to delete:\n");
-			scanf("%d", &key);
-			AccomodationsT* deleted = find(key);
-			if (deleted != NULL) {
-				deleteAccommodations(&head, key);
-			}
-			else {
-				printf("No entry with that ID could be found!\n\n");
-			}			
+		else if (menuChoice == 2) {//delete an accommodation
+			deleteAccommodation();
 		}
-		else if (menuChoice == 3) {//display accomodations
-			printf("Please enter the ID of the accommodation you are looking for:\n");
-			scanf("%d", &key);
-			AccomodationsT* found = find(key);
-			if (found != NULL) {
-				displayAccommodations(head);
-			}
-			else {
-				printf("No entry with that ID could be found!\n\n");
-			}
-			
+		else if (menuChoice == 3) {//display all accommodation
+			displayAll();
 		}
-		else if (menuChoice == 4) {//update existing accomodation
-			updateAccommodations(&head);
+		else if (menuChoice == 4) {//display specific accommodation
+			displaySpecific();
 		}
-		else if (menuChoice == 5) {//display general stats of an accomodation
-			generalStats(&head); 
+		else if (menuChoice == 5) {//update an accommodation
+			updateAccommodation();
 		}
-		else if (menuChoice == 6) {//list the monthly rental income of accomodations
-			listRental(&head);
+		else if (menuChoice == 6) {//save to file
+			saveToFile(totalNodes);
+		}
+		else if (menuChoice == 7) {
+			getTotalRooms();
 		}
 		else if (menuChoice != -1) {
-			printf("\nYou have entered an invalid option, please try again!\n\n");
+			printf("The value you have entered is incorrect, please try again!\n");
+			break;
 		}
 
-		printf("Enter 1 to add a new accommodation:\n");
-		printf("Enter 2 to delete an existing accommodation:\n");
-		printf("Enter 3 to display an accommodation:\n");
-		printf("Enter 4 to update an existing accommodation:\n");
-		printf("Enter 5 to list the general stats of an accommodation:\n");
-		printf("Enter 6 to list the monthly rental income of an accommodation:\n");
+		printf("\n\nEnter 1 to add a new accommodation\n");
+		printf("Enter 2 to delete an existing accommodation\n");
+		printf("Enter 3 to display all accommodation\n");
+		printf("Enter 4 to display a specific accommodation\n");
+		printf("Enter 5 to update an existing accommodation\n");
+		printf("Enter 6 to save all accommodations to a file\n");
 		scanf("%d", &menuChoice);
 	}
 }
 
-void AddNodeData(AccomodationsT** first, int id, char n[20], char s[20], int year, float monthlyRent, char e[50], char accomAvailable[20], int roomAvailable, bool pet, int duration) {
-	AccomodationsT* newNode = (AccomodationsT*)malloc(sizeof(AccomodationsT) * 1);
-	newNode->idNum = id;
-	strcpy(newNode->firstName, n);
-	strcpy(newNode->surName, s);
-	strcpy(newNode->email, e);
-	newNode->year = year;
-	newNode->monthlyRental = monthlyRent;
-	strcpy(newNode->accomAvailable, accomAvailable);
-	newNode->roomAvailable = roomAvailable;
-	newNode->pets = pet;
-	newNode->stayDuration = duration;
+void insert() {
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node) * 1);
+	totalNodes++;
+	newNode->idNum = totalNodes;
+	newNode->next = NULL;
 
-	newNode->next = *first;
-	*first = newNode;
+	printf("\nPlease enter the First Name:\n");
+	scanf("%s", newNode->firstName);
+
+	printf("Please enter the Sirname:\n");
+	scanf("%s", newNode->surName);
+
+	printf("Please enter the year it was built:\n");
+	scanf("%d", &newNode->year);
+
+	printf("Please enter the monthly rental income:\n");
+	scanf("%f", &newNode->monthlyRental);
+
+	printf("Please enter the email:\n");
+	scanf("%s", newNode->email);
+
+	printf("Please enter the type of accommodation:\n");
+	scanf("%s", &newNode->accomAvailable);
+
+	printf("please enter how many rooms are available:\n");
+	scanf("%d", &newNode->roomAvailable);
+
+	printf("Please enter if pets are allowed(1 = yes or 0 = no):\n");
+	scanf("%d", &newNode->pets);
+
+	printf("Please enter the duration of the stay:\n\n");
+	scanf("%d", &newNode->stayDuration);
+
+	if (head == NULL) {
+		head = newNode;
+	}
+	else {
+		struct Node* temp = head;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+
+		temp->next = newNode;
+		newNode->next = NULL;
+	}
 }
 
-void saveToFile(AccomodationsT** first) {
-
-}
-
-void addAccommodation(AccomodationsT** first) {
-
-}
-
-struct node* find(int key) {
+struct node* find(int target) {
 	//start from the first link
-	AccomodationsT* current = head;
+	struct Node* current = head;
 
 	//if list is empty
 	if (head == NULL) {
@@ -131,7 +188,7 @@ struct node* find(int key) {
 	}
 
 	//navigate through list
-	while (current->idNum != key) {
+	while (current->idNum != target) {
 
 		//if it is last node
 		if (current->next == NULL) {
@@ -147,35 +204,91 @@ struct node* find(int key) {
 	return current;
 }
 
-void displayAccommodations(AccomodationsT** first) {
-	AccomodationsT* temp;
+void displayAll() {
+	struct Node* temp = head;
 
-	temp = first;
 	while (temp != NULL) {
-		printf("\n\nID Number: %d", temp->idNum);
-		printf("\nName: %s %s", temp->firstName, temp->surName);
-		printf("\nYear Built: %d", temp->year);
-		printf("\nMonthly Rental Income: %.2f", temp->monthlyRental);
-		printf("\nEmail: %s", temp->email);
-		printf("\nAvailable accommodation: %s", temp->accomAvailable);
-		printf("\nRooms Available: %d", temp->roomAvailable);
-		printf("\nPets Allowed: %s", temp->pets ? "YES" : "NO");
-		printf("\nDuration of stay: %d", temp->stayDuration);
+		display(temp);
+		temp = temp->next;
+	}
+}
 
-		if (temp == NULL) {
-			printf("There is no accommodation data available.\n");
+void displaySpecific() {
+	int id;
+	printf("What is the id of the accommodation you would like to find?\n");
+	scanf("%d", &id);
+	if (id != 0) {
+		struct Node* tmp = find(id);
+		if (tmp == NULL) {
+			printf("\nThat is not an available ID, please try again\n");
+		}
+		else {
+			display(tmp);
 		}
 	}
 }
 
-void updateAccommodations(AccomodationsT** first) {
-
+void display(struct Node* temp) {
+	printf("\n\nID Number: %d", temp->idNum);
+	printf("\nName: %s %s", temp->firstName, temp->surName);
+	printf("\nYear Built: %d", temp->year);
+	printf("\nMonthly Rental Income: %.2f", temp->monthlyRental);
+	printf("\nEmail: %s", temp->email);
+	printf("\nAvailable accommodation: %s", temp->accomAvailable);
+	printf("\nRooms Available: %d", temp->roomAvailable);
+	printf("\nPets Allowed: %s", temp->pets == 1 ? "YES" : "NO");
+	printf("\nDuration of stay: %d", temp->stayDuration);
 }
 
-void deleteAccommodations(AccomodationsT** first, int key) {
+void updateAccommodation() {
+	int id;
+	printf("What is the id of the accommodation you would like to find?\n");
+	scanf("%d", &id);
+	if (id != 0) {
+		struct Node* tmp = find(id);
+		if (tmp == NULL) {
+			printf("\nThat is not an available ID, please try again\n");
+		}
+		else {
+
+			printf("\nPlease enter the First Name:\n");
+			scanf("%s", tmp->firstName);
+
+			printf("Please enter the Sirname:\n");
+			scanf("%s", tmp->surName);
+
+			printf("Please enter the year it was built:\n");
+			scanf("%d", &tmp->year);
+
+			printf("Please enter the monthly rental income:\n");
+			scanf("%f", &tmp->monthlyRental);
+
+			printf("Please enter the email:\n");
+			scanf("%s", tmp->email);
+
+			printf("Please enter the type of accommodation:\n");
+			scanf("%s", &tmp->accomAvailable);
+
+			printf("please enter how many rooms are available:\n");
+			scanf("%d", &tmp->roomAvailable);
+
+			printf("Please enter if pets are allowed(1 = yes or 0 = no):\n");
+			scanf("%d", &tmp->pets);
+
+			printf("Please enter the duration of the stay:\n\n");
+			scanf("%d", &tmp->stayDuration);
+		}
+		printf("Accommdation was successfully updated!");
+	}
+}
+
+void deleteAccommodation() {
+	int key;
+	printf("Enter the ID of the accommodation you wish to delete:\n");
+	scanf("%d", &key);
 	//start from the first link
-	AccomodationsT* current = head;
-	AccomodationsT* previous = NULL;
+	struct Node* current = head;
+	struct Node* previous = NULL;
 
 	//if list is empty
 	if (head == NULL) {
@@ -210,10 +323,35 @@ void deleteAccommodations(AccomodationsT** first, int key) {
 	return current;
 }
 
-void generalStats(AccomodationsT** first) {
+void saveToFile() {
+	FILE* fp;
 
+	struct Node* temp = head;
+	fp = fopen("accommodations.txt", "a");
+
+	if (fp == NULL) {
+		printf("Cannot open file!\n");
+	}
+	else {
+		while (temp != NULL) {
+			fprintf(fp, "%d|%s|%s|%d|%.2f|%s|%s|%d|%d|%d\n", temp->idNum, temp->firstName, temp->surName, temp->year, temp->monthlyRental, temp->email, temp->accomAvailable, temp->roomAvailable, temp->pets, temp->stayDuration);
+			temp = temp->next;
+		}
+		fclose(fp);
+		printf("All Accommodations were saved!\n");
+	}
 }
 
-void listRental(AccomodationsT** first) {
+int getTotalRooms() {
+	int totalRooms = 0;
+	int singleRooms = 0;
 
+	struct Node* temp = head;
+
+	while (temp != NULL) {
+		totalRooms += temp->roomAvailable;
+		temp = temp->next;
+	}
+
+	return totalRooms / 100 * singleRooms;
 }
